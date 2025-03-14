88a3c810b90d77359171e5157113ece0d1bab1b9
rameev17 <rameev17@gmail.com>
2025-03-15 00:32:53 +0500

    fix: disable infinite loop in ShopCarousel for improved performance

--- src/components/Home/Shop/ShopCarousel.jsx
+++ src/components/Home/Shop/ShopCarousel.jsx
@@ -69,7 +69,7 @@ const ShopCarousel = ({ data, setSelectedItem, setIsOpen }) => {
     <StyledCarousel
       showArrows={true}
       autoPlay={true}
-      infiniteLoop={true}
+      infiniteLoop={false}
       showThumbs={false}
       showStatus={false}
       interval={3000}
